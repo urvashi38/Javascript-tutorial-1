5cc0ded9b0f8cb4ef12bdee9654dae71240918ab
Urvashi  Kushwaha <136898711+urvashi38@users.noreply.github.com>
2026-06-16 01:32:38 +0000

    datatypes and variables

---
+++ basics1/datatypes.js
@@ -0,0 +1,8 @@
+//"use strict"; // trate all js code newer version
+
+let name = "hitesh"
+let age = 16
+let isLoggedIn = false
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ basics1/variables.js
@@ -0,0 +1,14 @@
+const accountId = 12234
+let accountEmail = "urvashikushwahagmail.com"
+var accountPassword = "1234"
+accountCity = "jhansi"
+
+//accountId = 2
+
+accountEmail = "urvashi.com"
+accountPassword = "9867"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity])
