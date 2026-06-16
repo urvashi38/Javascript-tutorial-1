34991ced2f8ce323649b62d2a9b5c3072413c133
Urvashi  Kushwaha <136898711+urvashi38@users.noreply.github.com>
2026-06-16 07:05:32 +0530

    Update datatypes.js

--- basics1/datatypes.js
+++ basics1/datatypes.js
@@ -4,5 +4,13 @@ let name = "hitesh"
 let age = 16
 let isLoggedIn = false
 
+//number
+//bigint
+//boolean
+//undefined
+//null
+//string
+//symbol
+
 console.log(typeof undefined); //undefined
 console.log(typeof null); //object
